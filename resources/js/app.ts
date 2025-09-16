d93cc519b15928426b0c881caa58e83796b8a907
adjsky <igorlfmartins@mail.ru>
2025-09-17 02:11:06 +0300

    setup inertia

---
+++ resources/js/app.ts
@@ -0,0 +1,21 @@
+import { createInertiaApp } from "@inertiajs/svelte";
+import { hydrate, mount } from "svelte";
+
+import type { ResolvedComponent } from "@inertiajs/svelte";
+
+void createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob<ResolvedComponent>(
+            "./pages/**/*.svelte",
+            { eager: true }
+        );
+        return pages[`./pages/${name}.svelte`];
+    },
+    setup({ el, App, props }) {
+        if (el && el.dataset.serverRendered === "true") {
+            hydrate(App, { target: el, props });
+        } else if (el) {
+            mount(App, { target: el, props });
+        }
+    }
+});
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/css/app.css", "resources/js/app.ts"],
+            refresh: true
         }),
-        tailwindcss(),
-    ],
+        svelte(),
+        tailwindcss()
+    ]
 });
