c16643f3daa80b7dcd60cbd1b162a060b3f0456f
adjsky <igorlfmartins@mail.ru>
2025-09-18 16:58:11 +0300

    add wayfinder

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { wayfinder } from "@laravel/vite-plugin-wayfinder";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import laravel from "laravel-vite-plugin";
@@ -5,11 +6,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
+        wayfinder(),
         laravel({
             input: ["resources/js/app.ts"],
             refresh: true
         }),
         svelte(),
         tailwindcss()
-    ]
+    ],
+    resolve: {
+        alias: {
+            $: "/resources/js"
+        }
+    }
 });
