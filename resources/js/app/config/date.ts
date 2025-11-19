af9e8e58ec1a5441e164eed10d119669a3fe586b
adjsky <igorlfmartins@mail.ru>
2025-11-19 18:52:42 +0300

    fix

--- resources/js/app/config/date.ts
+++ resources/js/app/config/date.ts
@@ -1,15 +1,17 @@
-import { getLocale } from "$/paraglide/runtime";
 import dayjs from "dayjs";
 import isoWeek from "dayjs/plugin/isoWeek";
 
-import type { Locale } from "$/paraglide/runtime";
+// TODO: remove when we can load locales lazily
+import "dayjs/locale/en";
+import "dayjs/locale/ru";
 
 dayjs.extend(isoWeek);
 
-const locales: Record<Locale, () => Promise<unknown>> = {
-    en: () => import("dayjs/locale/en"),
-    ru: () => import("dayjs/locale/ru")
-};
-
 // TODO: find out how to lazily load dayjs locales
+//
+// const locales: Record<Locale, () => Promise<unknown>> = {
+//     en: () => import("dayjs/locale/en"),
+//     ru: () => import("dayjs/locale/ru")
+// };
+//
 // await locales[getLocale()]();
