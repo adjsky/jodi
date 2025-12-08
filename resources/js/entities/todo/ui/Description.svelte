319ce683e10492dbbd329cdfd1e34cc8defcb650
adjsky <igorlfmartins@mail.ru>
2025-12-08 15:51:52 +0300

    make description autoresizeable

--- resources/js/entities/todo/ui/Description.svelte
+++ resources/js/entities/todo/ui/Description.svelte
@@ -9,7 +9,7 @@
 
 <Field.Textarea
     {...props}
-    rows={6}
-    class="mt-6 form-input field-sizing-content w-full resize-none border-none bg-transparent p-0 text-lg font-semibold text-cream-950 placeholder:text-cream-600 focus:ring-0"
+    autoresize
+    class="mt-6 form-input field-sizing-content w-full border-none bg-transparent p-0 text-lg font-semibold text-cream-950 placeholder:text-cream-600 focus:ring-0"
     placeholder={m["todos.placeholders.description"]()}
 />
