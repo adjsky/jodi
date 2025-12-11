64e195e8b8a76159cbd6284847e119a266a2bc22
adjsky <igorlfmartins@mail.ru>
2025-12-11 16:39:27 +0300

    tweak friends

--- resources/js/pages/CurrentUser/Friends.svelte
+++ resources/js/pages/CurrentUser/Friends.svelte
@@ -3,6 +3,7 @@
     import { User } from "$/entities/user";
     import { m } from "$/paraglide/messages";
     import Jelly from "$/shared/assets/jelly.svg";
+    import Button from "$/shared/ui/Button.svelte";
 
     type Props = {
         friends: App.Data.FriendDto[];
@@ -12,42 +13,49 @@
 </script>
 
 <SettingsLayout title={m["current-user.account.friends"]()}>
-    <div
-        class={[
-            "flex flex-col py-5",
-            friends.length == 0 && "justify-center",
-            friends.length > 0 && "gap-2"
-        ]}
-    >
-        {#if friends.length == 0}
-            <img
-                src={Jelly}
-                width={82}
-                height={85}
-                alt=""
-                loading="lazy"
-                decoding="async"
-                class="mx-auto max-w-32"
-            />
-            <p class="mx-auto mt-4 max-w-3/4 text-center text-lg font-medium">
-                {m["current-user.friends.no-friends"]()}
-            </p>
-        {:else}
-            {#each friends as friend (friend.id)}
-                <div
-                    class="border-gray-950 flex items-center gap-3 rounded-xl border bg-white px-4 py-3"
+    <div class="flex flex-grow flex-col justify-between py-5">
+        <div
+            class={[
+                "flex flex-grow flex-col pb-5",
+                friends.length == 0 && "justify-center",
+                friends.length > 0 && "gap-2"
+            ]}
+        >
+            {#if friends.length == 0}
+                <img
+                    src={Jelly}
+                    width={82}
+                    height={85}
+                    alt=""
+                    loading="lazy"
+                    decoding="async"
+                    class="mx-auto max-w-32"
+                />
+                <p
+                    class="mx-auto mt-4 max-w-3/4 text-center text-lg font-medium"
                 >
-                    <User.Avatar name={friend.name} />
-                    <div>
-                        <p class="font-semibold">
-                            {friend.name}
-                        </p>
-                        <p class="text-sm text-cream-400">
-                            {friend.email}
-                        </p>
+                    {m["current-user.friends.no-friends"]()}
+                </p>
+            {:else}
+                {#each friends as friend (friend.id)}
+                    <div
+                        class="border-gray-950 flex items-center gap-3 rounded-xl border bg-white px-4 py-3"
+                    >
+                        <User.Avatar name={friend.name} />
+                        <div>
+                            <p class="font-semibold">
+                                {friend.name}
+                            </p>
+                            <p class="text-sm text-cream-400">
+                                {friend.email}
+                            </p>
+                        </div>
                     </div>
-                </div>
-            {/each}
-        {/if}
+                {/each}
+            {/if}
+        </div>
+        <Button disabled>
+            {m["current-user.add"]()}
+        </Button>
     </div>
 </SettingsLayout>
