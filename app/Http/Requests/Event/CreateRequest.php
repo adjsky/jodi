bcc28cc204cc2d3e71dc76bb64a570561c3bb4e0
adjsky <igorlfmartins@mail.ru>
2025-12-30 05:35:22 +0300

    fix validation

--- app/Http/Requests/Event/CreateRequest.php
+++ app/Http/Requests/Event/CreateRequest.php
@@ -25,7 +25,7 @@ class CreateRequest extends FormRequest
             'color' => 'nullable|hex_color',
             'startsAt' => 'required|date',
             'endsAt' => 'required|date',
-            'notifyAt' => 'sometimes|date',
+            'notifyAt' => 'nullable|date',
         ];
     }
 }
