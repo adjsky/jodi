21cd098af819006a7febf786e2095cd7db4ca057
adjsky <igorlfmartins@mail.ru>
2026-01-08 01:30:59 +0300

    fix: theme color mismatch

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
     <link rel="icon" href="/favicon.ico">
     <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml">
     <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" sizes="180x180">
-    <meta name="theme-color" content="#f67a3c">
+    <meta name="theme-color" content="#fdf3e2">
 
     @production
         <link rel="manifest" href="/build/manifest.webmanifest">
