9c7a59bf49d7b80adb6b245d12d0c641dc50308e
adjsky <igorlfmartins@mail.ru>
2026-02-06 17:35:04 +0300

    fix: add any to maskable icon

--- vite.config.js
+++ vite.config.js
@@ -77,7 +77,7 @@ export default defineConfig({
                         src: "/maskable-icon-512x512.png",
                         sizes: "512x512",
                         type: "image/png",
-                        purpose: "maskable"
+                        purpose: "any maskable"
                     },
                     {
                         src: "/monochrome-icon-96x96.png",
