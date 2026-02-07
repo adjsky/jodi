0c8e91735fc57fd45ff52934db21d3b0601e86ec
adjsky <igorlfmartins@mail.ru>
2026-02-07 04:28:41 +0300

    fix: await listener

--- resources/js/app/config/capacitor.ts
+++ resources/js/app/config/capacitor.ts
@@ -2,7 +2,7 @@ import { App } from "@capacitor/app";
 import { Capacitor } from "@capacitor/core";
 
 if (Capacitor.isNativePlatform()) {
-    void App.addListener("backButton", (e) => {
+    await App.addListener("backButton", (e) => {
         if (e.canGoBack) {
             window.history.back();
         } else {
