bce4a99e31e577eed62135511931bae90f129b23
adjsky <igorlfmartins@mail.ru>
2026-02-08 01:05:35 +0300

    fix: add dummy folder for capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,7 @@ dotenv.config({ quiet: true });
 const config: CapacitorConfig = {
     appId: process.env.CAPACITOR_APP_ID,
     appName: "Jodi",
+    webDir: "resources/capacitor",
     server: {
         url: process.env.CAPACITOR_SERVER_URL,
         cleartext: !process.env.CAPACITOR_SERVER_URL?.startsWith("https://")
