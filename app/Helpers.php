79380c880c44b08802fdf7f46e3ebce16e2a464c
adjsky <igorlfmartins@mail.ru>
2026-05-12 00:39:19 +0300

    fix: gemini review

--- app/Helpers.php
+++ app/Helpers.php
@@ -6,8 +6,8 @@ use RRule\RRule;
 
 function rrules_match(string|RRule $rrule1, string|RRule $rrule2): bool
 {
-    $rrule1 = gettype($rrule1) === 'string' ? new RRule($rrule1) : $rrule1;
-    $rrule2 = gettype($rrule2) === 'string' ? new RRule($rrule2) : $rrule2;
+    $rrule1 = is_string($rrule1) ? new RRule($rrule1) : $rrule1;
+    $rrule2 = is_string($rrule2) ? new RRule($rrule2) : $rrule2;
 
     return $rrule1->rfcString() === $rrule2->rfcString();
 }
