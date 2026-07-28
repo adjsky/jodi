1ce57f8dac17be58861f5a499058b1c98200b0ed
adjsky <igorlfmartins@mail.ru>
2026-07-29 02:40:01 +0300

    fix: preserve web vibrations in selectionChanged

--- resources/js/shared/lib/haptics/selection-changed.ts
+++ resources/js/shared/lib/haptics/selection-changed.ts
@@ -1,9 +1,14 @@
 import { Haptics } from "@capawesome/capacitor-haptics";
+import { PLATFORM } from "$/shared/cfg/constants";
 
 import { isAvailable } from "./is-avaiable";
 
 export async function selectionChanged() {
     if (!(await isAvailable())) return;
 
-    void Haptics.selectionChanged();
+    if (PLATFORM == "web") {
+        navigator.vibrate?.(10);
+    } else {
+        void Haptics.selectionChanged();
+    }
 }
