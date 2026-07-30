5bb3dc8ed5447cd22e4f2cb1bf7b42cfeace45d2
adjsky <igorlfmartins@mail.ru>
2026-07-30 23:57:10 +0300

    fix: disable zoom

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover">
 
     <title>{{ config('app.name') }}</title>
     <meta name="description" content="Journal & Diary">
